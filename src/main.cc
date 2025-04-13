595145d68c2c9851ae80f84417bafaa2bc669c58
Devel User <bliang11@g.ucla.edu>
2025-04-13 07:13:11 +0000

    Refactor server functions
    Change-Id: I487a4865c7981037f12d33c8084010a1c95a4137

---
+++ src/main.cc
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include "server.h"
+#include "session.h"
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    using namespace std; // For atoi.
+    server s(io_service, atoi(argv[1]));
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
---
+++ src/server.h
@@ -0,0 +1,24 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "session.h"
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class server
+{
+public:
+  server(boost::asio::io_service& io_service, short port);
+
+private:
+  void start_accept();
+
+  void handle_accept(session* new_session,
+    const boost::system::error_code& error);
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+#endif
