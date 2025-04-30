5b74fbaf9c7eb76785978f079dc940f72a7ba484
Soo Hyun Mun <suhyunmoon1998@g.ucla.edu>
2025-04-29 23:52:11 -0700

    Logger
    Change-Id: I3457e749b1db6d3a8400e3fc1452b943b997f8da

---
+++ include/logger.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/support/date_time.hpp>
+
+void init_logging();
--- src/logger.cc
+++ src/logger.cc
@@ -3,34 +3,36 @@
 #include <boost/log/expressions/formatters/stream.hpp>
 #include <boost/log/expressions/formatters/if.hpp>
 #include <boost/log/expressions/formatter.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/core.hpp>
+
 namespace logging = boost::log;
 namespace expr = boost::log::expressions;
 namespace keywords = boost::log::keywords;
-void init_logging() {
-    logging::add_common_attributes();
-    // Console output
-    logging::add_console_log(
-        std::cout,
-        keywords::format = (
-            expr::stream
-                << "[" << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S")
-                << "] [Thread " << expr::attr<boost::log::attributes::current_thread_id::value_type>("ThreadID")
-                << "] [" << logging::trivial::severity
-                << "] " << expr::smessage
-        )
-    );
-    // File output with rotation
-    logging::add_file_log(
-        keywords::file_name = "logs/server_%Y-%m-%d_%H-%M-%S.log",
-        keywords::rotation_size = 10 * 1024 * 1024, // 10MB
-        keywords::time_based_rotation = logging::sinks::file::rotation_at_time_point(0, 0, 0), // Rotate at midnight
-        keywords::format = (
-            expr::stream
-                << "[" << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S")
-                << "] [Thread " << expr::attr<boost::log::attributes::current_thread_id::value_type>("ThreadID")
-                << "] [" << logging::trivial::severity
-                << "] " << expr::smessage
-        )
-    );
-}
 
+void init_logging() {
+    static bool initialized = false;
+    if (initialized) return;
+    initialized = true;
+
+    logging::add_common_attributes();
+
+    logging::add_file_log(
+        keywords::file_name = "logs/server.log",        
+        keywords::open_mode = std::ios_base::app,
+        keywords::format = (
+            expr::stream
+                << "[" << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S")
+                << "] [Thread " << expr::attr<boost::log::attributes::current_thread_id::value_type>("ThreadID")
+                << "] [" << logging::trivial::severity
+                << "] " << expr::smessage
+        )
+    );
+    
+    logging::core::get()->set_filter(
+        logging::trivial::severity >= logging::trivial::info
+    );
+
+    BOOST_LOG_TRIVIAL(info) << "starting up";
+}
